fd409620d93ea2a800b2a293b78e28156961e811
moliveros <mcoliveros1202@gmail.com>
2022-06-04 09:22:11 -0500

    added buttons to filter language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -44,6 +45,22 @@ var getUserRepos = function(user) {
     });
 };
 
+// LESSON 5 CODE BEGIN
+var getFeaturedRepos = function(language) {
+  var apiUrl = "http://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language)
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+// LESSON 5 CODE END
+
 var displayRepos = function(repos, searchTerm) {
   // check if api returned any repos
   if (repos.length === 0) {
@@ -61,7 +78,6 @@ var displayRepos = function(repos, searchTerm) {
     // create a container for each repo
     var repoEl = document.createElement('a');
     repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
     repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
@@ -91,5 +107,18 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language")
+  getFeaturedRepos("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
